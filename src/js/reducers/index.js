7cbb43a148e2f0068141f7529f19112830af4b51
nacam403 <satnakam@gmail.com>
2018-01-11 20:11:20 +0900

    Simplify reducer by object rest spread

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -5,32 +5,37 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
   case 'FETCH_TODOS':
-    return Object.assign({}, state, {
+    return {
+      ...state,
       todos: action.todos
-    });
+    };
 
   case 'ADD_TODO':
-    return Object.assign({}, state, {
+    return {
+      ...state,
       todos: [
         ...state.todos,
         action.todo
       ]
-    });
+    };
 
   case 'START_EDIT_TODO':
-    return Object.assign({}, state, {
-      todos: state.todos.map(todo => (todo.id !== action.id) ? todo : Object.assign({}, todo, { editing: true }))
-    });
+    return {
+      ...state,
+      todos: state.todos.map(todo => (todo.id !== action.id) ? todo : { ...todo, editing: true })
+    };
 
   case 'UPDATE_TODO':
-    return Object.assign({}, state, {
+    return {
+      ...state,
       todos: state.todos.map(todo => (todo.id !== action.todo.id) ? todo : action.todo)
-    });
+    };
 
   case 'DELETE_TODO':
-    return Object.assign({}, state, {
+    return {
+      ...state,
       todos: state.todos.filter(todo => todo.id !== action.id)
-    });
+    };
 
   default:
     return state;
